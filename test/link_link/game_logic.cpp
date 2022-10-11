dc27413657a41ab3b812b109e34efdc34b81847b
Jiarui Wang <wangjiarui4@outlook.com>
2022-10-11 13:44:05 +0800

    test(game_logic): score

--- test/link_link/game_logic.cpp
+++ test/link_link/game_logic.cpp
@@ -1,5 +1,11 @@
+#include "link_link/block/blank.h"
+#include "link_link/block/diamond.h"
+#include "link_link/block/special.h"
+#include "link_link/block/wall.h"
 #include "link_link/link_link.h"
 #include <QtTest>
+using namespace std;
+using namespace link_link::block;
 class GameLogicTest : public QObject {
     Q_OBJECT
 private:
@@ -60,15 +66,94 @@ private slots:
         // Paused
         engine.togglePaused();
         engine.elapse(1);
-        QVERIFY(engine.getGameTimeLeft() == 99);
+        QVERIFY(engine.getGameTimeLeft() == 100);
         engine.togglePaused();
         // Time up
-        engine.elapse(99);
+        engine.elapse(100);
         QVERIFY(engine.getGameTimeLeft() == 0);
         QVERIFY(engine.isGameEnd());
         QVERIFY(engine.isGameSolvable());
         engine.elapse(1);
         QVERIFY(engine.getGameTimeLeft() == 0);
+    }
+
+    void check_solvable()
+    {
+        engine.reset();
+        engine.set_map({
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(), make_shared<Blank>(),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(), make_shared<Blank>(),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>()},
+        });
+        QVERIFY(engine.isGameSolvable() == false);
+
+        engine.set_map({
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(), make_shared<Blank>(),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(),
+           make_shared<Special>(SpecialType::Flash), make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>()},
+        });
+        QVERIFY(engine.isGameSolvable() == true);
+
+        engine.set_map({
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(), make_shared<Blank>(),
+           make_shared<Wall>()},
+          {make_shared<Wall>(),
+           make_shared<Diamond>(Color::Blue, Shape::Square),
+           make_shared<Diamond>(Color::Blue, Shape::Square),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>()},
+        });
+        QVERIFY(engine.isGameSolvable() == true);
+        engine.set_map({
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(),
+           make_shared<Diamond>(Color::Blue, Shape::Square),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Diamond>(Color::Red, Shape::Square),
+           make_shared<Diamond>(Color::Blue, Shape::Square),
+           make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>()},
+        });
+        QVERIFY(engine.isGameSolvable() == false);
+    }
+
+    void check_get_score()
+    {
+        engine.reset();
+        engine.set_map({
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(), make_shared<Blank>(),
+           make_shared<Blank>(), make_shared<Blank>(), make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(), make_shared<Diamond>(Color::Red, Shape::Square),
+           make_shared<Diamond>(Color::Red, Shape::Square),make_shared<Blank>(), make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(), make_shared<Blank>(),
+           make_shared<Blank>(), make_shared<Blank>(), make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Blank>(), make_shared<Blank>(),
+           make_shared<Blank>(), make_shared<Blank>(), make_shared<Wall>()},
+          {make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>(),
+           make_shared<Wall>(), make_shared<Wall>(), make_shared<Wall>()},
+        });
+        engine.manipulate(Qt::Key_D);
+        engine.manipulate(Qt::Key_S);
+        engine.manipulate(Qt::Key_D);
+        engine.manipulate(Qt::Key_S);
+        QVERIFY(engine.getP1Score() == 1);
     }
 
 public:
